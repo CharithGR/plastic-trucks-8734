70f9fa2cf1ac24ba1204de1ea0ffbffe5b61f690
shashi1049 <shashi290396@gmail.com>
2023-02-22 23:44:17 +0530

    booking service

--- OnlineTripManagementSystem/src/main/java/com/masai/service/BookingService.java
+++ OnlineTripManagementSystem/src/main/java/com/masai/service/BookingService.java
@@ -1,8 +1,22 @@
 package com.masai.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import com.masai.exceptions.BookingException;
+import com.masai.exceptions.CustomerException;
+import com.masai.exceptions.HotelException;
+import com.masai.exceptions.PackageException;
+import com.masai.models.Booking;
 
 @Service
 public interface BookingService {
+	
+	
+	public Booking makeBooking(Booking booking)throws BookingException;
+	public Booking cancelBooking(Integer bookingId,Integer customerId)throws BookingException;
+	public Booking viewBooking(int bookingId)throws BookingException;
+	public List<Booking> viewAllBooking()throws BookingException ;
 
 }
